6a8a151d3aac841ebe7a27a43f0f8844cbae61f3
KeithY14 <keithyong14@yahoo.com.sg>
2021-10-12 17:34:49 +0800

    Trigger coin collision and score and time text

--- Assets/CoinScript.cs
+++ Assets/CoinScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CoinScript : MonoBehaviour
 {
+    private AudioSource audiosource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audiosource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject, 0.5f);
+            audiosource.Play();
+        }
     }
 }
